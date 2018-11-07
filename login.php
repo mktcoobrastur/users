a0c5b735cefe9c89588e89b2df998bd678f9df65
Leonardo Colombo <designer@coobrastur.net>
2018-11-07 17:14:15 -0200

    envio

--- login.php
+++ login.php
@@ -1,16 +1,23 @@
 <?php
-if(isset($_POST['submit'])) {
     $user   = $_POST['user'];
-
     require "../televenda/conn.php";
-    $consulta = mysqli_query($con, "SELECT * FROM atendentes Where id = '$user'");
-    $l = mysqli_fetch_array($consulta);
-    echo $l['qnt_vendas'];
-    
-}
+    if(isset($_POST['submit'])) {
+        $consulta = mysqli_query($con, "SELECT * FROM atendentes Where id = '$user'");
+        $l = mysqli_fetch_array($consulta);
+        echo $l['qnt_vendas'];
+        echo $user;
+    }
+    if(isset($_POST['submitCad'])) {
+        $consulta = mysqli_query($con, "SELECT * FROM atendentes Where id = '$user'");
+        $l = mysqli_fetch_array($consulta);
+        $qntVendas = $l['qnt_vendas'];
+        $qntFinal = $qntVendas + 1;
+        mysqli_query($con, "UPDATE atendentes SET qnt_vendas = $qntFinal WHERE id = $user");
+        echo $user;
+    }
 ?>
 
-<form action="" method="post">
+<form action="login.php" method="post">
     <div class="form-control">
         <select name="user" class="campo">
             <option>Escolha um Plano</option>
@@ -18,5 +25,6 @@ if(isset($_POST['submit'])) {
             <option value="1">VIP</option>
             <option value="2">GOLD MASTER</option>
         </select>
+        <input type="submit" name="submitCad" value="Lançar">
     </div>
 </form>
